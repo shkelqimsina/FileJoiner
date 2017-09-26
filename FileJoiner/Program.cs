1f71b7d4f293b76f3ca92067b3f2706d3cf8eced
Shkelqim Sina <Shkelqim Sina>
2017-09-26 17:52:08 +0200

    final commit

---
+++ FileJoiner/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace FileJoiner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] Files = ListFiles(args[0]);
+
+            for (int i = 0; i < args.Length; i++)
+            {
+                // First argument is the directory location
+                if (i == 0)
+                    continue;
+                File.WriteAllText(
+                    args[0] + "\\" + args[i],
+                    JoinFiles(Files, Path.GetExtension(args[i]))
+                    );
+            }
+        }
+
+        public static string JoinFiles(IList<string> Files, string Extension)
+        {
+            string all = "";
+            foreach (string file in Files)
+            {
+                if (Path.GetExtension(file) == Extension)
+                {
+                    TextReader tr = new StreamReader(file);
+                    string content = "";
+                    while (true)
+                    {
+                        string tmp = tr.ReadLine();
+                        if (tmp == null)
+                            break;
+                        else
+                            content += tmp + "\n";
+                    }
+                    tr.Close();
+                    tr = null;
+                    all += content;
+                    if (Extension == ".css" && content.Contains("@import"))
+                        Console.WriteLine("Import links are not considered!");
+                }
+            }
+            return all;
+        }
+
+        static string[] ListFiles(string directory)
+        {
+            string[] f = Directory.GetFiles(directory);
+            return f;
+        }
+    }
+}
